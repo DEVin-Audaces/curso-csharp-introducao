47f48b8f94b5aaed4f03a4a29cca4aabfd041a18
Kelvis Rodrigues Borges <kelvisbcc@gmail.com>
2022-06-10 00:46:05 -0300

    add class 2 activity

---
+++ Atividades/Aula02/Program.cs
@@ -0,0 +1,62 @@
+﻿static class Program
+{
+    static void Main(string[] args)
+    {
+        /**
+        * Exercicio 1
+        */
+        // Console.Write("Informe o primeiro valor: ");
+        // int valor1 = int.Parse(Console.ReadLine());
+
+        // Console.Write("Informe o segundo valor: ");
+        // int valor2 = int.Parse(Console.ReadLine());
+
+        // int resultadoSoma = valor1 + valor2;
+
+        // Console.Write("O resultado da soma é: " + resultadoSoma);
+
+        /**
+        * Exercicio 2
+        */
+        // System.Console.WriteLine("\n\n----------------- PACIENTE --------------\n");
+        // Console.Write("Informe o nome do paciente: ");
+        // string nome = Console.ReadLine();
+
+        // Console.Write("Informe a idade do paciente: ");
+        // int idade = int.Parse(Console.ReadLine());
+
+        // if (idade >= 60)
+        // {
+        //     Console.WriteLine($"{nome} é idoso.");
+        // }
+        // else if (idade >= 18)
+        // {
+        //     Console.WriteLine($"{nome} é maior de idade.");
+        // }
+        // else
+        // {
+        //     Console.WriteLine($"{nome} é menor de idade.");
+        // }
+
+        /**
+        * Exercicio 3
+        */
+        // System.Console.WriteLine("\n\n----------------- CARRO --------------\n");
+        // Console.Write("Informe o modelo do carro: ");
+        // string modelo = Console.ReadLine();
+
+        // Console.Write("Informe a marca do carro: ");
+        // string marca = Console.ReadLine();
+
+        // Console.Write("Informe o KM rodados: ");
+        // int kmRodados = int.Parse(Console.ReadLine());
+
+        // Console.WriteLine($"Seu carro é um {modelo} da marca {marca} e rodou {kmRodados}KM.");
+
+        // int kmRevisao = 10000;
+        // if (kmRodados > kmRevisao)
+        // {
+        //     Console.WriteLine($"Seu carro precisa de revisão.");
+        // }
+    }
+}
